c9d543b31a13637150666b3ffbff9775b091c229
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2019-10-20 23:58:33 +0400

    Implement to create a post with comments
    When the post data is loaded into Post model, create an array of
    comments and load `comments` array data also.
    Save comments after the post is saved

--- frontend/resource/Post.php
+++ frontend/resource/Post.php
@@ -16,6 +16,11 @@ namespace frontend\resource;
  */
 class Post extends \common\models\Post
 {
+    /**
+     * @author Zura Sekhniashvili <zurasekhniashvili@gmail.com>
+     * @var Comment[]
+     */
+    public $postComments = [];
 
     public function extraFields()
     {
@@ -28,5 +33,38 @@ class Post extends \common\models\Post
     public function getComments()
     {
         return $this->hasMany(Comment::class, ['post_id' => 'id']);
+    }
+
+    public function load($data, $formName = null)
+    {
+        $this->postComments = [];
+        if ($this->isNewRecord && isset($data['comments'])) {
+
+            foreach ($data['comments'] as $commentData) {
+                $comment = new Comment();
+                $comment->load($commentData, "");
+                $this->postComments[] = $comment;
+            }
+        }
+        return parent::load($data, $formName);
+    }
+
+    public function save($runValidation = true, $attributeNames = null)
+    {
+        $transaction = \Yii::$app->db->beginTransaction();
+        $wasNewRecord = $this->isNewRecord;
+        $result = parent::save($runValidation, $attributeNames);
+        if ($wasNewRecord && $this->postComments) {
+            foreach ($this->postComments as $comment) {
+                $comment->post_id = $this->id;
+                if (!$comment->save()) {
+                    $this->addError('comments', array_values($comment->errors));
+                    $transaction->rollBack();
+                    return false;
+                }
+            }
+        }
+        $transaction->commit();
+        return $result;
     }
 }
